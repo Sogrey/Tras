2959dd4b113409cc0c6b684f1e4a0966e5dd0041
Sogrey <suzhaoheng2006@163.com>
2015-01-26 14:26:23 +0800

    commit

---
+++ src/org/sogrey/tras/TopActivity.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.sogrey.tras;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author Administrator
+ *
+ */
+public class TopActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.top);
+	}
+
+}
